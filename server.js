abf958943cd211fe34c75744cabcd2542e662ce9
Ubuntu <ubuntu@ip-172-31-46-62.eu-west-2.compute.internal>
2025-06-14 11:23:04 +0000

    Updated gbeng-portfolio file

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/send', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'sorinolag@gmail.com',
+      pass: 'xgka rkwa wptp bjtl'
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'sorinolag@gmail.com',
+    subject: 'Portfolio Form Message',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) return res.status(500).send('Error');
+    res.send('Message sent!');
+  });
+});
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log(`Server running on http://127.0.0.1:3000`);
+});
